8d50ab68c36d11b242de3104e6b2f46c4514c984
Vovchik <vova2000ra@mail.ru>
2023-02-12 05:46:15 +0600

    item

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,6 @@ if (pageListItem) {
     pageListItem.textContent = h1.textContent;
 }
 
-
-
 var swiper2 = new Swiper(".my-swiper2", {
     spaceBetween: 5,
     slidesPerView: 5,
@@ -40,4 +38,30 @@ var swiper = new Swiper(".my-swiper", {
     thumbs: {
         swiper: swiper2,
     },
+});
+
+var canvases = document.querySelectorAll(".border-dashed");
+
+canvases.forEach(function (canvas) {
+  var ctx = canvas.getContext("2d");
+
+  function draw() {
+    var dashLen = 34;
+    var dashGap = 34;
+    var dashColor = "#EE7F01";
+    var bgColor = "#000000";
+    var x = 0;
+    var y = 0;
+    var width = 680;
+    var height = 480;
+
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = dashColor;
+    ctx.lineWidth = 30;
+    ctx.setLineDash([dashLen, dashGap]);
+    ctx.strokeRect(x, y, width, height);
+  }
+
+  draw();
 });
