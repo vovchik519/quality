9f5cb375e1e17c92a31ad7ec3a4890cc6f1c1bee
Vovchik <vova2000ra@mail.ru>
2023-02-12 14:07:29 +0600

    about

---
+++ assets/js/border.js
@@ -0,0 +1,51 @@
+var canvases = document.querySelectorAll(".border-dashed-680");
+
+canvases.forEach(function (canvas) {
+    var ctx = canvas.getContext("2d");
+
+    function draw() {
+        var dashLen = 34;
+        var dashGap = 34;
+        var dashColor = "#EE7F01";
+        var bgColor = "#000000";
+        var x = 0;
+        var y = 0;
+        var width = 680;
+        var height = 480;
+
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = dashColor;
+        ctx.lineWidth = 30;
+        ctx.setLineDash([dashLen, dashGap]);
+        ctx.strokeRect(x, y, width, height);
+    }
+
+    draw();
+});
+
+var canvases = document.querySelectorAll(".border-dashed-280");
+
+canvases.forEach(function (canvas) {
+    var ctx = canvas.getContext("2d");
+
+    function draw() {
+        var dashLen = 34;
+        var dashGap = 34;
+        var dashColor = "#EE7F01";
+        var bgColor = "#000000";
+        var x = 0;
+        var y = 0;
+        var width = 280;
+        var height = 295;
+
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = dashColor;
+        ctx.lineWidth = 30;
+        ctx.setLineDash([dashLen, dashGap]);
+        ctx.strokeRect(x, y, width, height);
+    }
+
+    draw();
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,29 +19,3 @@ document.title = h1.textContent + " - Официальный сайт произ
 if (pageListItem) {
     pageListItem.textContent = h1.textContent;
 }
-
-var canvases = document.querySelectorAll(".border-dashed");
-
-canvases.forEach(function (canvas) {
-  var ctx = canvas.getContext("2d");
-
-  function draw() {
-    var dashLen = 34;
-    var dashGap = 34;
-    var dashColor = "#EE7F01";
-    var bgColor = "#000000";
-    var x = 0;
-    var y = 0;
-    var width = 680;
-    var height = 480;
-
-    ctx.fillStyle = bgColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = dashColor;
-    ctx.lineWidth = 30;
-    ctx.setLineDash([dashLen, dashGap]);
-    ctx.strokeRect(x, y, width, height);
-  }
-
-  draw();
-});
