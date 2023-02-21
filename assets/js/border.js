a5cd9743c87bdd18f62e30e9d7554871bffd656a
Vovchik <vova2000ra@mail.ru>
2023-02-21 10:19:27 +0600

    border

--- assets/js/border.js
+++ assets/js/border.js
@@ -75,3 +75,29 @@ canvases.forEach(function (canvas) {
 
     draw();
 });
+
+var canvases = document.querySelectorAll(".border-dashed-144");
+
+canvases.forEach(function (canvas) {
+    var ctx = canvas.getContext("2d");
+
+    function draw() {
+        var dashLen = 34;
+        var dashGap = 34;
+        var dashColor = "#EE7F01";
+        var bgColor = "#000000";
+        var x = 0;
+        var y = 0;
+        var width = 144;
+        var height = 268;
+
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = dashColor;
+        ctx.lineWidth = 30;
+        ctx.setLineDash([dashLen, dashGap]);
+        ctx.strokeRect(x, y, width, height);
+    }
+
+    draw();
+});
